910dc919fb591809754edec9fd17033336d165fd
AnujinLhagvasuren <144761548+AnujinLhagvasuren@users.noreply.github.com>
2023-12-09 22:56:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,10 +57,10 @@ st.title(":bar_chart:  Animal Disease Dashboard")
 st.markdown("##")
 
 # TOP KPI's
-total_sales = int(df_selection["Total"].sum())
-average_rating = round(df_selection["Rating"].mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0))
-average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+total_number = int(df_selection["Гаралт"].sum())
+average_sick= round(df_selection["Өвчилсөн"].mean(), 1)
+star_rating = ":star:" * int(round(average_sick, 0))
+average_number_by_location = round(df_selection["Гаралт"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
